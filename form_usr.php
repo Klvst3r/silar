d0715415156fa109dbee60c9f04c793b11af6c4f
Klvst3r <klvst3r@yahoo.com>
2019-12-10 10:47:56 -0600

    Crete Reg Usr

---
+++ form_usr.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+	include "assets/header.html";
+	require "assets/menu.html";
+	include "assets/panel-med_init.html";
+	/*--------------Inicia Contenido----------------------*/
+	?>
+	<legend>Registro del Usuario</legend>
+
+	<!-- HTML -->
+
+	<!-- HTML -->
+
+	<?php
+
+	/*PHP*/
+
+	/*PHP*/
+
+
+
+	/*--------------Finaliza Contenido----------------------*/
+	include "assets/panel-med_final.html";
+	include ("assets/footer.html");
+?>
